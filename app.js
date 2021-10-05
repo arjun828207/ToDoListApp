3187d4d535853b3c5ff17d82ea1cedb92f4329f9
Arjun Pandey <arjun_bp@rediffmail.com>
2021-10-05 17:59:33 +0530

    core logic done

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app=express();
+app.use(bodyParser.urlencoded({ extended:true}));
 app.set('view engine', 'ejs');
+var newItem=[];
 app.get('/', function(req, res){
-   
- var today="x"
- 
- 
-   res.render("list",{nameofday:today});
-
+   var today=new Date();
+   var options={
+       weekday:"long",
+       day:"numeric",
+       month:"long"
+   };
+   var day=today.toLocaleDateString("en-us",options);
+   res.render("list",{nameofday:day,newItem:newItem});
+})
+app.post("/",function(req,res){
+  var x=req.body.newItem;
+  newItem.push(x);
+  res.redirect("/");
 })
 app.listen(3000,function() {
     console.log('listening on port 3000');
